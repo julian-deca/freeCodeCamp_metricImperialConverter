dd9439eac1f99ed2c4e6d8848382afb16f0b00e2
julian <juliandecastelli@gmail.com>
2022-08-16 19:21:22 -0300

    all test pass

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -34,7 +34,28 @@ suite("Functional Tests", function () {
       .get("/api/convert?input=3/7.2/4kg")
       .end(function (err, res) {
         assert.equal(res.status, 200);
-        assert.equal(res.text, "invalid unit");
+        assert.equal(res.text, "invalid number");
+      });
+  });
+  test("#convert 4", () => {
+    chai
+      .request(server)
+      .get("/api/convert?input=3/7.2/4kilomegagram")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "invalid number and unit");
+      });
+  });
+  test("#convert 5", () => {
+    chai
+      .request(server)
+      .get("/api/convert?input=kg")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(
+          res.text,
+          '{"initNum":1,"initUnit":"kg","returnNum":2.20462,"returnUnit":"lbs","string":"1 kilograms converts to 2.20462 pounds"}'
+        );
       });
   });
 });
